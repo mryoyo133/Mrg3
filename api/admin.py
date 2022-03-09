7b075fef9a99b2e5d79de2ae8e9c2e10a27df1aa
mryoyo133 <mryoyo133@gmail.com>
2022-03-09 22:51:00 +0300

    making the database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Subject, Test, Question
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Test)
+admin.site.register(Question)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
+# Create your models here.
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    tests = models.ManyToManyField('Test')
+
+class Subject(models.Model):
+    name = models.CharField(max_length= 24)
+    tests = models.ManyToManyField('Test', blank = True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Test(models.Model):
+    title = models.CharField(max_length= 24, blank= True)
+    questions = models.ManyToManyField('Question')
+    def __str__(self):
+        return f'{self.title}'
+    
+class Question(models.Model):
+    question_body = models.CharField(max_length=300)
+    choice_1 = models.CharField(max_length=64, blank=True)
+    choice_2 = models.CharField(max_length=64, blank=True)
+    choice_3 = models.CharField(max_length=64, blank=True)
+    choice_4 = models.CharField(max_length=64, blank=True)
+    answer =  models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return f'{self.question_body}'
+
